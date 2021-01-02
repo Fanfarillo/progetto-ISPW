04bef0edb32e4a9f5098917e7d3c94ad4d4f5c28
luca-capotombolo <66474292+luca-capotombolo@users.noreply.github.com>
2021-01-02 23:14:44 +0100

    JSP servlet

---
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/Web/ServletAdviceView.java
@@ -0,0 +1,49 @@
+package com.capo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletAdviceView extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String b1 = req.getParameter("home");
+		String b2 = req.getParameter("manageMenu");
+		String b3 = req.getParameter("sponsorRestaurant");
+		String b4 = req.getParameter("back");
+		String b5 = req.getParameter("continue");
+		
+		
+		
+		if(b1!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("home");
+		}
+		if(b2!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("manageMenu");
+		}
+		if(b3!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("sponsor");
+		}
+		if(b4!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("back");
+		}
+		if(b5!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("continue");
+		}
+	}
+
+}
---
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/Web/ServletRestaurantMenuView.java
@@ -0,0 +1,67 @@
+package com.capo;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletRestaurantMenuView extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String b1 = req.getParameter("delete");
+		String b2 = req.getParameter("modify");
+		String b3 = req.getParameter("add");
+		String b4 = req.getParameter("advice");
+		String b5 = req.getParameter("home");
+		String b6 = req.getParameter("manageMenu");
+		String b7 = req.getParameter("sponsorRestaurant");
+		String b8 = req.getParameter("back");
+		
+		
+		req.setAttribute("nome", "Matteo");
+		
+		
+		
+		if(b1!=null) {
+			req.getRequestDispatcher("DeleteDishView.jsp").forward(req, resp);
+			System.out.print("delete");
+		}
+		if(b2!=null) {
+			req.getRequestDispatcher("ModifyDishView.jsp").forward(req, resp);
+			System.out.print("modify");
+		}
+		if(b3!=null) {
+			req.getRequestDispatcher("AddDishView.jsp").forward(req, resp);
+			System.out.print("add");
+		}
+		if(b4!=null) {
+			req.getRequestDispatcher("AdviceView.jsp").forward(req, resp);
+			System.out.print("advice");
+		}
+		if(b5!=null) {
+			req.getRequestDispatcher("DeleteDishView.jsp").forward(req, resp);
+			System.out.print("home");
+		}
+		if(b6!=null) {
+			req.getRequestDispatcher("RestaurantMenuview.jsp").forward(req, resp);
+			System.out.print("manage");
+		}
+		if(b7!=null) {
+			req.getRequestDispatcher("AddDishView.jsp").forward(req, resp);
+			System.out.print("sponsor");
+		}
+		if(b8!=null) {
+			req.getRequestDispatcher("AdviceView.jsp").forward(req, resp);
+			System.out.print("back");
+		}
+		
+	}
+	
+}
