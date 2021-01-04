f77330bf71a8b9ad1b9196d665a65a74b7f9d88e
MegAtonBoom <40306263+MegAtonBoom@users.noreply.github.com>
2021-01-04 18:12:17 +0100

    Fixato bug generalizzazione controller  4/01/2021 18:11

--- Fanfa's documents/Coding_originale/EaTypical/src/logic/view/standalone/View.java
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/view/standalone/View.java
@@ -14,7 +14,7 @@ public class View extends Application {
 	
 	@Override
 	public void start(Stage arg0) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("ScheduleTrip/TripSettingsView.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Chooserestaurant/ItalianViewcity.fxml"));
 		Parent rootParent = loader.load();
 		Scene scene = new Scene(rootParent);
 		arg0.setScene(scene);
