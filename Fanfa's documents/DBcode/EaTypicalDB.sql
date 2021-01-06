4e530b747aa8e1c68ea1da601af4c1e813527e09
luca-capotombolo <66474292+luca-capotombolo@users.noreply.github.com>
2021-01-06 17:29:13 +0100

    LC 06/01/2021 17:28 MODIFICA DB

--- Fanfa's documents/DBcode/EaTypicalDB.sql
+++ Fanfa's documents/DBcode/EaTypicalDB.sql
@@ -14,93 +14,89 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema progettoispwfinaledb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `progettoispwfinaledb` DEFAULT CHARACTER SET utf8 ;
-USE `progettoispwfinaledb` ;
+CREATE SCHEMA IF NOT EXISTS `progettoispwfinaledatabase` DEFAULT CHARACTER SET utf8 ;
+USE `progettoispwfinaledatabase` ;
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Proprietario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Proprietario` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Proprietario` (
   `Cognome` VARCHAR(45) NOT NULL,
   `Username` VARCHAR(45) NOT NULL,
   `Password` VARCHAR(45) NOT NULL,
   `Nome` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Cognome`, `Nome`),
-  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE)
+   PRIMARY KEY(`Username`)
+  -- PRIMARY KEY (`Cognome`, `Nome`),
+  -- UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE)
+  )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Ristorante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Ristorante` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Ristorante` (
   `Nome` VARCHAR(45) NOT NULL,
   `Indirizzo` VARCHAR(45) NOT NULL,
   `Citta` VARCHAR(45) NOT NULL,
-  `NomeProprietario` VARCHAR(45) NOT NULL,
-  `CognomeProprietario` VARCHAR(45) NOT NULL,
+  `UsernameProprietario` VARCHAR(45) NOT NULL,
+  -- `NomeProprietario` VARCHAR(45) NOT NULL,
+  -- `CognomeProprietario` VARCHAR(45) NOT NULL,
   `VotoMedio` DOUBLE NOT NULL,
   PRIMARY KEY (`Nome`),
-  INDEX `fk_nomeP_idx` (`NomeProprietario` ASC) VISIBLE,
-  INDEX `fk_cognomeP_idx` (`CognomeProprietario` ASC) VISIBLE,
+  INDEX `fk_nomeP_idx` (`UsernameProprietario` ASC) VISIBLE, 
   CONSTRAINT `fk_nomeP`
-    FOREIGN KEY (`NomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Nome`)
+    FOREIGN KEY (`UsernameProprietario`)
+    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cognomeP`
-    FOREIGN KEY (`CognomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE NO ACTION
+ )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Turista`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Turista` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Turista` (
   `Nome` VARCHAR(45) NOT NULL,
   `Cognome` VARCHAR(45) NOT NULL,
   `Username` VARCHAR(45) NOT NULL,
   `Password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Nome`, `Cognome`),
-  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE)
+  PRIMARY KEY(`Username`)
+  -- PRIMARY KEY (`Nome`, `Cognome`),
+  -- UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE)
+  )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Preferiti`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Preferiti` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Preferiti` (
   `NomeRistorante` VARCHAR(45) NOT NULL,
-  `NomeTurista` VARCHAR(45) NOT NULL,
-  `CognomeTurista` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`NomeRistorante`, `NomeTurista`, `CognomeTurista`),
-  INDEX `fk_nomeTur_idx` (`NomeTurista` ASC) VISIBLE,
-  INDEX `fk_cognomeTur_idx` (`CognomeTurista` ASC) VISIBLE,
+  `UsernameTurista` VARCHAR(45) NOT NULL,
+  -- `NomeTurista` VARCHAR(45) NOT NULL,
+  -- `CognomeTurista` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`NomeRistorante`, `UsernameTurista`),
+  INDEX `fk_nomeTur_idx` (`UsernameTurista` ASC) VISIBLE,  
   CONSTRAINT `fk_nomeRist`
     FOREIGN KEY (`NomeRistorante`)
     REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_nomeTur`
-    FOREIGN KEY (`NomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Nome`)
+    FOREIGN KEY (`UsernameTurista`)
+    REFERENCES `progettoispwfinaledb`.`Turista` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cognomeTur`
-    FOREIGN KEY (`CognomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE NO ACTION
+  )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Piatto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Piatto` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Piatto` (
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `NomePiatto` VARCHAR(45) NOT NULL,
   `Contenuto` VARCHAR(10000) NOT NULL,
@@ -119,40 +115,31 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`NotificaScheduling`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`NotificaScheduling` (
-  `NomeProprietario` VARCHAR(45) NOT NULL,
-  `CognomeProprietario` VARCHAR(45) NOT NULL,
-  `NomeTurista` VARCHAR(45) NOT NULL,
-  `CognomeTurista` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`NotificaScheduling` (
+  `UsernameProprietario` VARCHAR(45) NOT NULL,
+  -- `NomeProprietario` VARCHAR(45) NOT NULL,
+  -- `CognomeProprietario` VARCHAR(45) NOT NULL,
+  `UsernameTurista` VARCHAR(45) NOT NULL,
+  -- `NomeTurista` VARCHAR(45) NOT NULL,
+  -- `CognomeTurista` VARCHAR(45) NOT NULL,
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `Data` DATE NULL,
   `PranzoVsCena` TINYINT NULL,
   `Vista` TINYINT NULL,
-  PRIMARY KEY (`NomeProprietario`, `CognomeProprietario`, `NomeTurista`, `CognomeTurista`, `NomeRistorante`),
-  INDEX `fNotifica2_idx` (`CognomeProprietario` ASC) VISIBLE,
-  INDEX `fNotifica3_idx` (`NomeTurista` ASC) VISIBLE,
-  INDEX `fNotifica4_idx` (`CognomeTurista` ASC) VISIBLE,
+  PRIMARY KEY (`UsernameProprietario`,`UsernameTurista`, `NomeRistorante`),
+  INDEX `fNotifica2_idx` (`UsernameProprietario` ASC) VISIBLE,
+  INDEX `fNotifica3_idx` (`UsernameTurista` ASC) VISIBLE, 
   INDEX `fNotifica5_idx` (`NomeRistorante` ASC) VISIBLE,
   CONSTRAINT `fNotifica1`
-    FOREIGN KEY (`NomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Nome`)
+    FOREIGN KEY (`UsernameProprietario`)
+    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fNotifica2`
-    FOREIGN KEY (`CognomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE NO ACTION,  
   CONSTRAINT `fNotifica3`
-    FOREIGN KEY (`NomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Nome`)
+    FOREIGN KEY (`UsernameTurista`)
+    REFERENCES `progettoispwfinaledb`.`Turista` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fNotifica4`
-    FOREIGN KEY (`CognomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE NO ACTION,  
   CONSTRAINT `fNotifica5`
     FOREIGN KEY (`NomeRistorante`)
     REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
@@ -164,25 +151,21 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Recensione`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Recensione` (
-  `NomeTurista` VARCHAR(45) NOT NULL,
-  `CognomeTurista` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Recensione` (
+  `UsernameTurista` VARCHAR(45) NOT NULL,
+  -- `NomeTurista` VARCHAR(45) NOT NULL,
+  -- `CognomeTurista` VARCHAR(45) NOT NULL,
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `Contenuto` VARCHAR(10000) NOT NULL,
   `Voto` INT NOT NULL,
-  PRIMARY KEY (`NomeTurista`, `CognomeTurista`, `NomeRistorante`),
-  INDEX `fRec2_idx` (`CognomeTurista` ASC) VISIBLE,
+  PRIMARY KEY (`UsernameTurista`, `NomeRistorante`),
+  INDEX `fRec2_idx` (`UsernameTurista` ASC) VISIBLE,
   INDEX `fRec3_idx` (`NomeRistorante` ASC) VISIBLE,
   CONSTRAINT `fRec1`
-    FOREIGN KEY (`NomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Nome`)
+    FOREIGN KEY (`UsernameTurista`)
+    REFERENCES `progettoispwfinaledb`.`Turista` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fRec2`
-    FOREIGN KEY (`CognomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE NO ACTION, 
   CONSTRAINT `fRec3`
     FOREIGN KEY (`NomeRistorante`)
     REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
@@ -194,24 +177,20 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`NotificaRecensione`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`NotificaRecensione` (
-  `NomeTurista` VARCHAR(45) NOT NULL,
-  `CognomeTurista` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`NotificaRecensione` (
+  `UsernameTurista` VARCHAR(45) NOT NULL,
+  -- `NomeTurista` VARCHAR(45) NOT NULL,
+  -- `CognomeTurista` VARCHAR(45) NOT NULL,
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `Visualizzata` TINYINT NULL,
-  PRIMARY KEY (`NomeTurista`, `CognomeTurista`, `NomeRistorante`),
-  INDEX `fNotR2_idx` (`CognomeTurista` ASC) VISIBLE,
+  PRIMARY KEY (`UsernameTurista`, `NomeRistorante`),
+  INDEX `fNotR2_idx` (`UsernameTurista` ASC) VISIBLE,
   INDEX `fNotR3_idx` (`NomeRistorante` ASC) VISIBLE,
   CONSTRAINT `fNotR1`
-    FOREIGN KEY (`NomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Nome`)
+    FOREIGN KEY (`UsernameTurista`)
+    REFERENCES `progettoispwfinaledb`.`Turista` (`Username`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fNotR2`
-    FOREIGN KEY (`CognomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Cognome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE NO ACTION, 
   CONSTRAINT `fNotR3`
     FOREIGN KEY (`NomeRistorante`)
     REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
@@ -223,21 +202,17 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`RistorantiSponsorizzati`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`RistorantiSponsorizzati` (
-  `NomeProprietario` VARCHAR(45) NOT NULL,
-  `CognomeProprietario` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`RistorantiSponsorizzati` (
+  `UsernameProprietario` VARCHAR(45) NOT NULL,
+  -- `NomeProprietario` VARCHAR(45) NOT NULL,
+  -- `CognomeProprietario` VARCHAR(45) NOT NULL,
   `NomeRistorante` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`NomeProprietario`, `CognomeProprietario`, `NomeRistorante`),
-  INDEX `fRS2_idx` (`CognomeProprietario` ASC) VISIBLE,
+  PRIMARY KEY (`UsernameProprietario`, `NomeRistorante`),
+  INDEX `fRS2_idx` (`UsernameProprietario` ASC) VISIBLE,
   INDEX `fRS3_idx` (`NomeRistorante` ASC) VISIBLE,
   CONSTRAINT `fRS1`
-    FOREIGN KEY (`NomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Nome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fRS2`
-    FOREIGN KEY (`CognomeProprietario`)
-    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Cognome`)
+    FOREIGN KEY (`UsernameProprietario`)
+    REFERENCES `progettoispwfinaledb`.`Proprietario` (`Username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fRS3`
@@ -251,44 +226,36 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`NotificaMenuAggiornato`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`NotificaMenuAggiornato` (
-  `NomeTurista` VARCHAR(45) NOT NULL,
-  `CognomeTurista` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`NotificaMenuAggiornato` (
+  `UsernameTurista` VARCHAR(45) NOT NULL,
+  -- `NomeTurista` VARCHAR(45) NOT NULL,
+  -- `CognomeTurista` VARCHAR(45) NOT NULL,
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `TIpoModifica` VARCHAR(45) NOT NULL,
   `NomePiatto` VARCHAR(45) NOT NULL,
   `Vista` TINYINT NOT NULL,
-  PRIMARY KEY (`NomeTurista`, `CognomeTurista`, `NomeRistorante`, `NomePiatto`),
-  INDEX `fMA2_idx` (`CognomeTurista` ASC) VISIBLE,
+  PRIMARY KEY (`UsernameTurista`, `NomeRistorante`, `NomePiatto`),
+  INDEX `fMA2_idx` (`UsernameTurista` ASC) VISIBLE,
   INDEX `fMA3_idx` (`NomeRistorante` ASC) VISIBLE,
   INDEX `fMA4_idx` (`NomePiatto` ASC) VISIBLE,
   CONSTRAINT `fMA1`
-    FOREIGN KEY (`NomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Nome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fMA2`
-    FOREIGN KEY (`CognomeTurista`)
-    REFERENCES `progettoispwfinaledb`.`Turista` (`Cognome`)
+    FOREIGN KEY (`UsernameTurista`)
+    REFERENCES `progettoispwfinaledb`.`Turista` (`Username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fMA3`
-    FOREIGN KEY (`NomeRistorante`)
-    REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
+    FOREIGN KEY (`NomeRistorante`,`NomePiatto`)
+    REFERENCES `progettoispwfinaledb`.`Piatto` (`NomeRistorante`,`NomePiatto`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fMA4`
-    FOREIGN KEY (`NomePiatto`)
-    REFERENCES `progettoispwfinaledb`.`Piatto` (`NomePiatto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE NO ACTION
+    )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`Menu`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`Menu` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`Menu` (
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `Totale` DOUBLE NOT NULL,
   PRIMARY KEY (`NomeRistorante`),
@@ -303,21 +270,17 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `progettoispwfinaledb`.`DettaglioMenu`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `progettoispwfinaledb`.`DettaglioMenu` (
+CREATE TABLE IF NOT EXISTS `progettoispwfinaledatabase`.`DettaglioMenu` (
   `NomeRistorante` VARCHAR(45) NOT NULL,
   `NomePiatto` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`NomeRistorante`, `NomePiatto`),
   INDEX `fDM1_idx` (`NomePiatto` ASC) VISIBLE,
   CONSTRAINT `fDM1`
-    FOREIGN KEY (`NomePiatto`)
-    REFERENCES `progettoispwfinaledb`.`Piatto` (`NomePiatto`)
+    FOREIGN KEY (`NomeRistorante`,`NomePiatto`)
+    REFERENCES `progettoispwfinaledb`.`Piatto` (`NomeRistorante`,`NomePiatto`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fDM2`
-    FOREIGN KEY (`NomeRistorante`)
-    REFERENCES `progettoispwfinaledb`.`Ristorante` (`Nome`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE NO ACTION
+    )
 ENGINE = InnoDB;
 
 
