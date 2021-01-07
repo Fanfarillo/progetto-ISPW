f1f75b6b0e777e526339bdea7adb157f64c59ffd
MegAtonBoom <40306263+MegAtonBoom@users.noreply.github.com>
2021-01-07 19:12:50 +0100

    ViewCity select city completata 19:11 7/1/2021

--- Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/ControllerGuiHomePageOwner.java
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/ControllerGuiHomePageOwner.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import logic.controller.guicontroller.ManageMenuGuiController.
 
 public class ControllerGuiHomePageOwner extends OwnerBaseGuiController {
 
