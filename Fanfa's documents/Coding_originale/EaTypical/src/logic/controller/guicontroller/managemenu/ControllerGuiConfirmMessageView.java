01478be00a298152e1b0c3b1d305fb093976eb8b
luca-capotombolo <66474292+luca-capotombolo@users.noreply.github.com>
2021-02-07 23:42:53 +0100

    manage menu standalone

---
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/managemenu/ControllerGuiConfirmMessageView.java
@@ -0,0 +1,175 @@
+/**
+ * Sample Skeleton for 'ConfirmMessageView.fxml' Controller Class
+ */
+
+
+package logic.controller.guicontroller.managemenu;
+
+import logic.controller.applicationcontroller.ManageMenu;
+import logic.controller.guicontroller.OwnerBaseGuiController;
+import logic.engineeringclasses.bean.manageMenu.BeanDeleteDish;
+import logic.engineeringclasses.bean.manageMenu.BeanDish;
+import logic.engineeringclasses.exceptions.DishAlreadyExists;
+import logic.engineeringclasses.exceptions.InvalidDishDelete;
+import logic.engineeringclasses.exceptions.InvalidDishModify;
+import logic.engineeringclasses.others.Session;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * @author Luca Capotombolo
+ */
+
+
+
+public class ControllerGuiConfirmMessageView  extends OwnerBaseGuiController{
+	
+	private static final String DESTINATION = "/logic/view/standalone/ManageRestaurant/RestaurantMenuView.fxml";
+	private BeanDeleteDish beanDeleteDish;
+	private BeanDish beanDish;	
+	
+	
+	public ControllerGuiConfirmMessageView(BeanDeleteDish beanDeleteDish,Session bs) {
+		super(bs);
+		this.beanDeleteDish = beanDeleteDish;
+		
+	}
+	
+	public ControllerGuiConfirmMessageView(BeanDish beanDish,Session bs) {
+		super(bs);
+		this.beanDish = beanDish;
+		
+	}
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+
+    @FXML // fx:id="nomeUtenteLabel"
+    private Label nomeUtenteLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="doneButton"
+    private Button doneButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="discardAllTheChangesButton"
+    private Button discardAllTheChangesButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="keepManagingMenuButton"
+    private Button keepManagingMenuButton; // Value injected by FXMLLoader
+
+    
+    /**
+     * Scarta la opzione e ritorna al menu principale
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void discardChanges(ActionEvent event) throws IOException {
+    	
+    	//scarto tutte le opzioni e ritorno al menu iniziale
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+    	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(bs));
+    	Parent root = loader.load();
+    	myAnchorPane.getChildren().setAll(root);
+    }
+
+    
+    /**
+     * Eseguo effettivamente l'operazione richiesta: aggiornameto, eliminazione o inserimento di un piatto
+     * @param event
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    @FXML
+    void done(ActionEvent event) throws ClassNotFoundException, IOException {
+    	ManageMenu manageMenu = new ManageMenu();
+    	//E' diverso da null se e' stata richiesta la modifica di un piatto o l'inserimento di un piatto
+    	if(beanDish!=null) {
+    		//verifico se e' stata richiesta una scrittura
+    		if(beanDish.getTypeChange()==0) {
+    			try {
+    				manageMenu.addDish(beanDish);
+				} catch (DishAlreadyExists e) {
+					FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+		        	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(0,e.getMess(),bs));
+		        	Parent root = loader.load();
+		        	myAnchorPane.getChildren().setAll(root);
+		        	return;
+				}
+    			
+    			
+    		}else {
+    			try {
+    				manageMenu.modifyDishes(beanDish);
+				} catch (InvalidDishModify e) {
+					FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+		        	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(1, e.getMess(),bs));
+		        	Parent root = loader.load();
+		        	myAnchorPane.getChildren().setAll(root);
+		        	return;
+				}    			
+    		}
+    	
+    	}
+    	
+    	//E' diversa da null se e' stata richiesta l'eliminazione di un piatto
+    	if(beanDeleteDish!=null) {
+    		try {
+    			manageMenu.deleteDish(beanDeleteDish);
+			} catch (InvalidDishDelete e) {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+	        	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(2, e.getMess(),bs));
+	        	Parent root = loader.load();
+	        	myAnchorPane.getChildren().setAll(root);
+	        	return;
+			}
+			
+    	}
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+		//se vale -1 allora non c'ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¨ stato alcun errore
+    	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(bs));
+    	Parent root = loader.load();
+    	myAnchorPane.getChildren().setAll(root);
+    	
+    }
+
+    
+    /**
+     * Reindirizzo al menu principale
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void keepManagingMenu(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource(DESTINATION));
+    	loader.setControllerFactory(c -> new ControllerGuiRestaurantMenuView(bs));
+    	Parent root = loader.load();
+    	myAnchorPane.getChildren().setAll(root);
+    }
+
+    
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert manageMenuButton != null : "fx:id=\"manageMenuButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert sponsorRestaurantButton != null : "fx:id=\"sponsorMenuButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert nomeUtenteLabel != null : "fx:id=\"nomeUtenteLabel\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert doneButton != null : "fx:id=\"doneButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert discardAllTheChangesButton != null : "fx:id=\"discardAllTheChangesButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        assert keepManagingMenuButton != null : "fx:id=\"keepManagingMenuButton\" was not injected: check your FXML file 'ConfirmMessageView.fxml'.";
+        nomeUtenteLabel.setText(bs.getUser().getUsername());
+    }
+}
---
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/controller/guicontroller/managemenu/ControllerGuiReviewNotificationsView.java
@@ -0,0 +1,110 @@
+/**
+ * Sample Skeleton for 'NotificationsRestaurantViewScheduling.fxml' Controller Class
+ */
+
+package logic.controller.guicontroller.managemenu;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import logic.controller.guicontroller.ControllerGuiHomePageOwner;
+import logic.controller.guicontroller.OwnerBaseGuiController;
+import logic.engineeringclasses.bean.manageMenu.BeanListReviews;
+import logic.engineeringclasses.bean.manageMenu.BeanReview;
+import logic.engineeringclasses.others.Session;
+
+public class ControllerGuiReviewNotificationsView extends OwnerBaseGuiController{
+
+	private BeanListReviews beanListReviews;
+	
+    public ControllerGuiReviewNotificationsView(Session bs,BeanListReviews beanListReviews) {
+		super(bs);
+		this.beanListReviews= beanListReviews; 
+	}
+
+	@FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="nomeUtente"
+    private Label nomeUtente; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tabellaNotifiche"
+    private TableView<BeanReview> tabellaNotifiche; // Value injected by FXMLLoader
+    
+    @FXML
+	private TableColumn<BeanReview, String> votoColonna;
+
+    @FXML // fx:id="turistaColonna"
+    private TableColumn<BeanReview,String> turistaColonna; // Value injected by FXMLLoader
+
+    @FXML // fx:id="ristoranteColonna"
+    private TableColumn<BeanReview,String> ristoranteColonna; // Value injected by FXMLLoader
+
+    @FXML // fx:id="contenutoColonna"
+    private TableColumn<BeanReview,String> contenutoColonna; // Value injected by FXMLLoader
+
+    @FXML // fx:id="continueButton"
+    private Button continueButton; // Value injected by FXMLLoader
+
+   
+
+    @FXML
+    void goToHome() throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/logic/view/standalone/HomePageOwnerView.fxml"));
+    	
+   	   //setto il nuovo controller grafico
+   	   loader.setControllerFactory(c -> new ControllerGuiHomePageOwner(bs));
+   	   Parent rootParent = loader.load();    	
+     	
+     	//cambio scena
+     	myAnchorPane.getChildren().setAll(rootParent);
+    }
+
+   
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+       
+        assert tabellaNotifiche != null : "fx:id=\"tabellaNotifiche\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert turistaColonna != null : "fx:id=\"turistaColonna\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert ristoranteColonna != null : "fx:id=\"ristoranteColonna\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert votoColonna != null : "fx:id=\"votoColonna\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert contenutoColonna != null : "fx:id=\"contenutoColonna\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert continueButton != null : "fx:id=\"continueButton\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert myAnchorPane != null : "fx:id=\"myAnchorPane\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert manageMenuButton != null : "fx:id=\"manageMenuButton\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert sponsorRestaurantButton != null : "fx:id=\"sponsorRestaurantButton\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        assert nomeUtente != null : "fx:id=\"nomeUtente\" was not injected: check your FXML file 'NotificationsRestaurantViewScheduling.fxml'.";
+        
+        nomeUtente.setText(bs.getUser().getUsername());
+        turistaColonna.setCellValueFactory(new PropertyValueFactory<>("tourist"));
+        ristoranteColonna.setCellValueFactory(new PropertyValueFactory<>("restaurant"));
+        contenutoColonna.setCellValueFactory(new PropertyValueFactory<>("content"));
+        votoColonna.setCellValueFactory(new PropertyValueFactory<>("voto"));
+        BeanReview beanReview;
+        
+        ObservableList<BeanReview> observableList = FXCollections.observableArrayList();
+        for(int i = 0; i<beanListReviews.getContents().size();i++) {
+        	beanReview = new BeanReview(beanListReviews.getTourists().get(i), beanListReviews.getRestaurants().get(i), beanListReviews.getContents().get(i), beanListReviews.getVotes().get(i));
+        	observableList.add(beanReview);        	
+        }
+        
+        tabellaNotifiche.setItems(observableList);
+    }
+}
