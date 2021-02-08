a9f4b75d3afcb07003a7e6a02bad3b2f6752fc7c
Fanfarillo <72879649+Fanfarillo@users.noreply.github.com>
2021-02-08 17:40:57 +0100

    Fanfa 08/02/2021 - 17:40

--- Fanfa's documents/Coding_originale/EaTypical/src/logic/view/standalone/View.java
+++ Fanfa's documents/Coding_originale/EaTypical/src/logic/view/standalone/View.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import logic.controller.guicontroller.ControllerGuiHomePageTourist;
 import logic.engineeringclasses.others.Session;
