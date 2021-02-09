65d5361878c44a071fc0a6d1d78d79edff40e3d3
Fanfarillo <72879649+Fanfarillo@users.noreply.github.com>
2021-02-09 17:04:23 +0100

    Fanfa 09/02/2021 - 17:04 Fixed some DB code

--- Fanfa's documents/DBcode/trigger/TriggerDB.sql
+++ Fanfa's documents/DBcode/trigger/TriggerDB.sql
@@ -9,21 +9,21 @@ BEGIN
     declare old_all_dishes_price float;
     declare new_all_dishes_price float;
     
-    set oldprezzo = old.prezzo;
-    set newprezzo = new.prezzo;
+    set oldprezzo = old.Prezzo;
+    set newprezzo = new.Prezzo;
     select count(*) into numdishes from piatto where NomeRistorante = old.NomeRistorante;
     select totale into oldtotale from menu where NomeRistorante = old.NomeRistorante;
     set old_all_dishes_price = oldtotale*numdishes/3;
     set new_all_dishes_price = old_all_dishes_price - oldprezzo + newprezzo;
     
-    update menu set totale = 3*new_all_dishes_price/numdishes where NomeRistorante = old.NomeRistorante;
+    update menu set totale = 3*new_all_dishes_price/numdishes where NomeRistorante = new.NomeRistorante;
 END!
 
 delimiter ;
 
 delimiter !
 
-CREATE TRIGGER `progettoispwfinaledatabase`.`trigger_add_dish` AFTER UPDATE ON `piatto` FOR EACH ROW
+CREATE TRIGGER `progettoispwfinaledatabase`.`trigger_add_dish` AFTER INSERT ON `piatto` FOR EACH ROW
 BEGIN
 	declare prezzo float;
     declare old_num_dishes int;
@@ -32,20 +32,27 @@ BEGIN
     declare old_all_dishes_price float;
     declare new_all_dishes_price float;
     
-    set prezzo = new.prezzo;
-    select count(*) into old_num_dishes from piatto where NomeRistorante = old.NomeRistornate;
-    set new_num_dishes = old_num_dishes+1;
-    select totale into oldtotale from menu where NomeRistorante = old.NomeRistorante;
-    set old_all_dishes_price = oldtotale*old_num_dishes/3;
+    set prezzo = new.Prezzo;
+    select count(*) into new_num_dishes from piatto where NomeRistorante = new.NomeRistorante;
+    set old_num_dishes = new_num_dishes-1;
+    
+    if old_num_dishes > 0 then
+		select totale into oldtotale from menu where NomeRistorante = new.NomeRistorante;
+        set old_all_dishes_price = oldtotale*old_num_dishes/3;
+	else
+		set oldtotale = 0;
+        set old_all_dishes_price = 0;
+	end if;
+
     set new_all_dishes_price = old_all_dishes_price + prezzo;
     
-    update menu set totale = 3*new_all_dishes_price/new_num_dishes where NomeRistorante = old.NomeRistorante;
+    update menu set totale = 3*new_all_dishes_price/new_num_dishes where NomeRistorante = new.NomeRistorante;
 END!
 
 delimiter ;
 delimiter !
 
-CREATE TRIGGER `progettoispwfinaledatabase`.`trigger_delete_dish` AFTER UPDATE ON `piatto` FOR EACH ROW
+CREATE TRIGGER `progettoispwfinaledatabase`.`trigger_delete_dish` AFTER DELETE ON `piatto` FOR EACH ROW
 BEGIN
 	declare prezzo float;
     declare old_num_dishes int;
@@ -54,9 +61,9 @@ BEGIN
     declare old_all_dishes_price float;
     declare new_all_dishes_price float;
     
-    set prezzo = old.prezzo;
-    select count(*) into old_num_dishes from piatto where NomeRistorante = old.NomeRistorante;
-    set new_num_dishes = old_num_dishes-1;
+    set prezzo = old.Prezzo;
+    select count(*) into new_num_dishes from piatto where NomeRistorante = old.NomeRistorante;
+    set old_num_dishes = new_num_dishes+1;
     select totale into oldtotale from menu where NomeRistorante = old.NomeRistorante;
     set old_all_dishes_price = oldtotale*old_num_dishes/3;
     set new_all_dishes_price = old_all_dishes_price - prezzo;
