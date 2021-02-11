1128fc4949bef4c2c3dcc08607f744503384b32a
luca-capotombolo <66474292+luca-capotombolo@users.noreply.github.com>
2021-02-11 13:24:42 +0100

    modifica selenium api manage menu

--- Fanfa's documents/Coding_originale/EaTypical/src/test/selenium/SeleniumManageMenu.java
+++ Fanfa's documents/Coding_originale/EaTypical/src/test/selenium/SeleniumManageMenu.java
@@ -1,60 +1,48 @@
 package test.selenium;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertEquals;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumManageMenu {
 	
-	public static void main(String arg[]) {
+	public static void main(String arg[]) throws InterruptedException {
 		SeleniumManageMenu seleniumManageMenu = new SeleniumManageMenu();
 		seleniumManageMenu.seleniumTest();
 	}
 
 	@Test
-	public void seleniumTest() {
+	public void seleniumTest() throws InterruptedException {
+		
+		
 		System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://localhost:8080/EaT_10_02_2021_1/HomePageTouristView.jsp");
 		driver.findElement(By.id("logout")).click();
 	    driver.findElement(By.id("usernametx")).click();
 	    driver.findElement(By.id("usernametx")).sendKeys("Elvira");
+	    TimeUnit.SECONDS.sleep(1);
 	    driver.findElement(By.id("passtx")).click();
 	    driver.findElement(By.id("passtx")).sendKeys("1");
+	    TimeUnit.SECONDS.sleep(1);
 	    driver.findElement(By.id("ownercb")).click();
+	    TimeUnit.SECONDS.sleep(1);
 	    driver.findElement(By.id("login")).click();
+	    TimeUnit.SECONDS.sleep(1);
 	    driver.findElement(By.id("Manage")).click();
-	    driver.findElement(By.id("delete")).click();
-	    
-	    
-	    
+	    TimeUnit.SECONDS.sleep(1);
+	    driver.findElement(By.xpath("//*[@id=\"delete\"]")).click();;    
+	    TimeUnit.SECONDS.sleep(1);
+	    driver.findElement(By.xpath("//*[@id=\"delete\"]")).click();
+	    TimeUnit.SECONDS.sleep(1);
+	    driver.findElement(By.xpath("//*[@id=\"Done\"]")).click();
+	    TimeUnit.SECONDS.sleep(1);
+	    String textError = driver.findElement(By.xpath("//*[@id=\"errore\"]")).getText();
 	
-	    
-	    
-	    
-	    
-	    WebDriverWait wait = new WebDriverWait(driver, 10);
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("piatto")));
-	    WebElement element = driver.findElement(By.name("piatto"));
-	    element.findElement(By.xpath("//option[. = 'Arrosticini']")).click();
-		WebElement element2 = driver.findElement(By.name("ristorante"));
-		element2.findElement(By.xpath("/html/body/div/form/div[3]/select/option[4]")).click();
-		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"delete\"]")));
-		driver.findElement(By.xpath("//*[@id=\"delete\"]")).click();
-		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"Done\"]")));
-		driver.findElement(By.xpath("//*[@id=\"Done\"]")).click();
-		
-		WebElement labelPath = driver.findElement(By.xpath("//*[@id=\"errore\"]"));
-		String labelContent = labelPath.getText();
-		
-		assertEquals("the inserted dish does not belong to the selected restaurant", labelContent);
+		assertEquals("the inserted dish does not belong to the selected restaurant", textError);
 		
 		driver.close();
 		
