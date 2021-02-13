97f5b23abc7878aed155ab554433d23b6163b055
Adrian Baba <40306263+MegAtonBoom@users.noreply.github.com>
2021-02-13 15:31:10 +0100

    selenium api test chooserest

---
+++ Fanfa's documents/Coding_originale/EaTypical/src/test/selenium/SeleniumChooseRestaurant.java
@@ -0,0 +1,44 @@
+package test.selenium;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumChooseRestaurant {
+
+	
+	@Test
+	public void seleniumTest() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://localhost:8080/EaTypical/HomePageTouristView.jsp");
+		
+		driver.findElement(By.xpath("//*[@id=\"logout\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		driver.findElement(By.xpath("//*[@id=\"usernametx\"]")).sendKeys("Pazizo");
+		driver.findElement(By.xpath("//*[@id=\"passtx\"]")).sendKeys("1");
+		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		driver.findElement(By.xpath("//*[@id=\"chooseRestaurant\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		driver.findElement(By.xpath("//*[@id=\"TO_img\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		driver.findElement(By.xpath("//*[@id=\"restaurantsScroll\"]")).sendKeys("Tre");
+		driver.findElement(By.xpath("//*[@id=\"updateInfo\"]")).click();
+		TimeUnit.SECONDS.sleep(1);
+		WebElement txtBoxContent= driver.findElement(By.xpath("//*[@id=\"addrtx\"]"));
+		String addressContent= txtBoxContent.getAttribute("value");
+		
+		assertEquals("IndirizzoTre", addressContent);
+		driver.close();
+		}
+}
